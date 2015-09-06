fd8a9bf122450fecdf505e2e871ee963dd8434f4
David Kirkby <dkirkby@uci.edu>
2015-09-06 08:15:45 -0700

    Fix import and tab issues

--- specsim/sources.py
+++ specsim/sources.py
@@ -4,10 +4,14 @@ Provides functions for creating different source spectra.
 """
 from __future__ import print_function, division
 
+import math
+
+import numpy as np
+
 root2pi = np.sqrt(2*math.pi)
 
 def getOIIDoubletFlux(totalFlux,wave):
-	"""
+    """
     Returns a vector of [OII] doublet fluxes in erg/(s*cm^2*Ang) corresponding to the
     input vector of rest-frame wavelengths in Angstroms. Uses the model of:
     https://desi.lbl.gov/trac/wiki/Pipeline/QuickSim#ELGs
@@ -24,32 +28,32 @@ lyaSpectra = None
 lyaZ = None
 
 def loadLyaSpectra(filename,zvec):
-	"""
-	Loads the Lya spectra tabulated at different redshifts from the specified file.
-	"""
-	# Load the tabulated wavelengths and fluxes from the file.
+    """
+    Loads the Lya spectra tabulated at different redshifts from the specified file.
+    """
+    # Load the tabulated wavelengths and fluxes from the file.
     data = np.loadtxt(lyaFilename,unpack=True)
     wave,fluxes = data[0],data[1:]
     assert len(zvec) == len(fluxes),(
-    	"loadLyaSpectra: redshift parameters do match file shape: %d,%d" %
-    	(len(zvec),len(fluxes)))
+        "loadLyaSpectra: redshift parameters do match file shape: %d,%d" %
+        (len(zvec),len(fluxes)))
     # Save the redshift vector.
-	lyaZ = numpy.array(zvec)
-	# Loop over redshifts and save a corresponding spectrum.
-	lyaSpectra = [ ]
-	for z in zvec:
+    lyaZ = numpy.array(zvec)
+    # Loop over redshifts and save a corresponding spectrum.
+    lyaSpectra = [ ]
+    for z in zvec:
         lyaSpectra.append(sim.SpectralFluxDensity(wave,fluxes[i],extrapolatedValue=0.))
 
 names = ['[OII]','LyaQSO']
 
 def createSource(name,redshift,totalFlux=None,magnitude=None,band='g'):
-	"""
-	Returns a SpectralFluxDensity object for the named source type at the
-	specified redshift. The flux normalization must be specified using either
-	the totalFlux in erg/(s*cm^2*Ang) or AB magnitude parameters, but not both.
-	The default magnitude is SDSS g band, but other SDSS bands can be specified
-	as long as they are covered by the underlying source model. The module
-	global sources.names lists the recognized source names.
-	"""
-	if name not in names:
-		raise RuntimeError('createSource: name %r not supported' % name)
+    """
+    Returns a SpectralFluxDensity object for the named source type at the
+    specified redshift. The flux normalization must be specified using either
+    the totalFlux in erg/(s*cm^2*Ang) or AB magnitude parameters, but not both.
+    The default magnitude is SDSS g band, but other SDSS bands can be specified
+    as long as they are covered by the underlying source model. The module
+    global sources.names lists the recognized source names.
+    """
+    if name not in names:
+        raise RuntimeError('createSource: name %r not supported' % name)

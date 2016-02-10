584baba419f55329279cb646f9ebab89fb7c8a7c
David Kirkby <dkirkby@uci.edu>
2016-02-10 10:24:35 -0800

    Add placeholder unit test files

---
+++ specsim/tests/test_spectrum.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function, division
+
+import numpy as np
+
+import astropy.units as u
+from astropy.tests.helper import pytest
+
+from ..spectrum import *
+
+
+def test_1():
+    pass

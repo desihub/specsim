e1095cbb4b35bff438c245eb7a03df5537870753
David Kirkby <dkirkby@uci.edu>
2016-02-17 16:02:42 -0800

    Add an update() method to Source

--- specsim/source.py
+++ specsim/source.py
@@ -5,6 +5,8 @@ from __future__ import print_function, division
 
 import numpy as np
 
+import scipy.interpolate
+
 import astropy.units as u
 
 
@@ -16,6 +18,32 @@ class Source(object):
         self.type_name = type_name
         self.wavelength = wavelength
         self.flux = flux
+
+
+    def update(self, type_name, wavelength, flux, extrapolate_value=None):
+        """
+        """
+        self.type_name = type_name
+        # Convert input arrays to original units.
+        try:
+            wavelength_value = wavelength.to(self.wavelength.unit).value
+        except AttributeError:
+            wavelength_value = np.asarray(wavelength)
+        try:
+            flux_value = flux.to(self.flux.unit).value
+        except AttributeError:
+            flux_value = np.asarray(flux)
+
+        # Interpolate to the simulation wavelengths if necessary.
+        if not np.array_equal(wavelength_value, self.wavelength.value):
+            bounds_error = extrapolate_value is None
+            interpolator = scipy.interpolate.interp1d(
+                wavelength_value, flux_value,
+                kind='linear', copy=False, assume_sorted=True,
+                bounds_error=bounds_error, fill_value=extrapolate_value)
+            flux_value = interpolator(self.wavelength.value)
+
+        self.flux = flux_value * self.flux.unit
 
 
 def initialize(config):

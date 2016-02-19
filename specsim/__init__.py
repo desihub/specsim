936a6b126ddd0462eac42dc024bf0f714c60c416
David Kirkby <dkirkby@uci.edu>
2016-02-19 13:53:22 -0800

    Purge __init__

--- specsim/__init__.py
+++ specsim/__init__.py
@@ -12,9 +12,4 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    import config
-    import atmosphere
-    import instrument
-    import source
-    import simulator
-    import transform
+    pass

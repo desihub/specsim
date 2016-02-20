c8a75eddefd367d9c3d03932ed860ab392f0e4ac
David Kirkby <dkirkby@uci.edu>
2016-02-20 14:58:50 -0800

    Add unit test for issue #1 (currently failing)

--- specsim/tests/test_simulator.py
+++ specsim/tests/test_simulator.py
@@ -15,3 +15,15 @@ def test_end_to_end():
     medsnr = np.median(results[results.obsflux > 0].snrtot)
     snrtot2 = np.sum(results.snrtot ** 2)
     assert np.allclose([medsnr, snrtot2], [2.10661167, 18068.7423])
+
+
+def test_zero_flux():
+    config = specsim.config.load_config('test')
+    sim = Simulator(config)
+    sim.source.update_in(
+        'Zero Flux', 'qso', sim.source.wavelength_in, 0 * sim.source.flux_in)
+    sim.source.update_out()
+    results = sim.simulate()
+    # Check that ivar is non-zero.
+    assert not np.all((results.camivar)[:, 0] == 0)
+    assert not np.all(results.ivar == 0)

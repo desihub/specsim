af11fe641e1c2e0bc7398321768a90c153ad0f7b
David Kirkby <dkirkby@uci.edu>
2016-10-14 16:51:16 -0700

    Add unit test for future sparse get_output_resolution_matrix()

--- specsim/tests/test_camera.py
+++ specsim/tests/test_camera.py
@@ -16,4 +16,24 @@ def test_resolution():
     c = specsim.config.load_config('test')
     i = specsim.instrument.initialize(c)
     R = i.cameras[0].get_output_resolution_matrix()
-    np.allclose(R.sum(0)[3:-3], 1)
+    assert np.allclose(R.sum(0)[3:-3], 1)
+
+
+def test_downsampling():
+    c = specsim.config.load_config('test')
+    i = specsim.instrument.initialize(c)
+    camera = i.cameras[0]
+
+    # Use an intermediate dense matrix for downsampling.
+    # This is the old implementation of get_output_resolution_matrix()
+    # that used too much memory.
+    n = len(camera._output_wavelength)
+    m = camera._downsampling
+    i0 = camera.ccd_slice.start - camera.response_slice.start
+    R1 = (camera._resolution_matrix[: n * m, i0 : i0 + n * m].toarray()
+         .reshape(n, m, n, m).sum(axis=3).sum(axis=1) / float(m))
+
+    # Use the new sparse implementation of get_output_resolution_matrix().
+    R2 = camera.get_output_resolution_matrix() #.toarray()
+
+    assert np.allclose(R1, R2)

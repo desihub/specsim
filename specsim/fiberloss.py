e80f0577cf34666df7ba071ee3816238d0f904a6
David Kirkby <dkirkby@uci.edu>
2016-10-18 10:00:13 -0700

    Build GalSim galaxy model

--- specsim/fiberloss.py
+++ specsim/fiberloss.py
@@ -6,9 +6,36 @@ illuminating a fiber and the on-sky aperture of the fiber.
 """
 from __future__ import print_function, division
 
+import astropy.units as u
+
 
 def calculate_fiber_acceptance_fraction(source, atmosphere, instrument,
                                         observation):
     """
     """
+    import galsim
+
+    # Create the source model, which we assume to be achromatic.
+    source_components = []
+    if source.disk_fraction > 0:
+        hlr = source.disk_shape.half_light_radius.to(u.arcsec).value
+        q = source.disk_shape.minor_major_axis_ratio
+        beta = source.disk_shape.position_angle.to(u.deg).value
+        disk_model = galsim.Exponential(
+            flux=source.disk_fraction, half_light_radius=hlr).shear(
+                q=q, beta=beta * galsim.degrees)
+    if source.disk_fraction < 1:
+        hlr = source.bulge_shape.half_light_radius.to(u.arcsec).value
+        q = source.bulge_shape.minor_major_axis_ratio
+        beta = source.bulge_shape.position_angle.to(u.deg).value
+        bulge_model = galsim.DeVaucouleurs(
+            flux=source.disk_fraction, half_light_radius=hlr).shear(
+                q=q, beta=beta * galsim.degrees)
+    if source.disk_fraction == 0:
+        source_model = bulge_model
+    elif source.disk_fraction == 1:
+        source_model = disk_model
+    else:
+        source_model = disk_model + bulge_model
+
     return instrument.get_fiber_acceptance(source)

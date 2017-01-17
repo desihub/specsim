a626a097d5ccd97bfd61a18f4c06d552d39a8520
David Kirkby <dkirkby@uci.edu>
2017-01-17 13:34:13 -0800

    Propagate name change to unit test

--- specsim/tests/test_quickspecsim.py
+++ specsim/tests/test_quickspecsim.py
@@ -1,9 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import print_function, division
 
-from ..driver import main
+from ..quickspecsim import main
 
 
-def test_driver(tmpdir):
+def test_quickspecsim(tmpdir):
     save = str(tmpdir.join('test.dat'))
     main('-c test -o {0}'.format(save).split())

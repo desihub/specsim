1c285e5a397e8234bddcbd14ee66a2d5972f2f82
Julien Guy <jguy@lbl.gov>
2017-09-29 17:09:56 -0700

    larger range of seeing and half light radii

--- specsim/fitgalsim.py
+++ specsim/fitgalsim.py
@@ -102,9 +102,9 @@ seed=0
 desi    = specsim.simulator.Simulator('desi') # the only one in package anyway ...
 wave    = np.linspace(3550.,10000,8) # Angstrom
 sources = ["POINT","DISK","BULGE"]
-nfibers = 20000 # random positions in focal plane (and possibly random source orientation)
-half_light_radii  = np.linspace(0.45,1.,3.) # half light radius in arcsec for disk=exponential and bulge=devaucouleurs profiles
-total_seeing_fwhm = np.linspace(1.,2.,3)  # FWHM in arcsec atmosphere + Mayall blur
+nfibers = 10000 # random positions in focal plane (and possibly random source orientation)
+half_light_radii  = np.linspace(0.3,1.5,5) # half light radius in arcsec for disk=exponential and bulge=devaucouleurs profiles
+total_seeing_fwhm = np.linspace(0.5,3.,5)  # FWHM in arcsec atmosphere + Mayall blur
 ########################################################################
 
 

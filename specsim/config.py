241ac2c6f7712730cf3fb110b52ec97f71adfe8b
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2021-10-29 16:00:04 -0700

    pkg_resources instead

--- specsim/config.py
+++ specsim/config.py
@@ -37,19 +37,12 @@ import scipy.interpolate
 
 import astropy.units
 import astropy.table
-import astropy.utils.data
 import astropy.coordinates
 import astropy.time
 import astropy.io.fits
 import astropy.wcs
 
-from astropy.utils.introspection import minversion
-ASTROPY_LT_4_3 = not minversion(astropy, '4.3')
-
-if ASTROPY_LT_4_3:
-    from astropy.utils.data import _find_pkg_data_path as get_pkg_data_path
-else:
-    from astropy.utils.data import get_pkg_data_path
+import pkg_resources
 
 def is_string(x):
     """Test if x is a string type.
@@ -245,7 +238,7 @@ class Configuration(Node):
         base_path = self.base_path
         if base_path == '<PACKAGE_DATA>':
             self._assign(
-                'abs_base_path', get_pkg_data_path('data'))
+                'abs_base_path', pkg_resources.resource_filename('specsim','data'))
         else:
             try:
                 self._assign('abs_base_path', base_path.format(**os.environ))
@@ -693,8 +686,7 @@ def load_config(name, config_type=Configuration):
     if extension:
         file_name = name
     else:
-        file_name = get_pkg_data_path(
-            'data/config/{0}.yaml'.format(name))
+        file_name  = pkg_resources.resource_filename('specsim','data/config/{0}.yaml'.format(name))
     if not os.path.isfile(file_name):
         raise ValueError('No such config file "{0}".'.format(file_name))
 
